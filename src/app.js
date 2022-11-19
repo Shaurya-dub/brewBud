8cef50b6d61c0141fd3f20689f7b7c4505073a63
Shaurya-dub <shauryadubey@hotmail.ca>
2022-11-19 13:09:19 -0500

    snapshot functionality established

--- src/app.js
+++ src/app.js
@@ -9,34 +9,42 @@
 const app = {};
 // import firebaseApp from "./firebase";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
-  import {
-    getDatabase,
-    ref,
-    set,
-  } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBOTN-ktAaGhBMYKy67ANjNF9MFPRLQeIY",
-    authDomain: "brewbud-80fd3.firebaseapp.com",
-    databaseURL: "https://brewbud-80fd3-default-rtdb.firebaseio.com",
-    projectId: "brewbud-80fd3",
-    storageBucket: "brewbud-80fd3.appspot.com",
-    messagingSenderId: "303106774801",
-    appId: "1:303106774801:web:6da98e2516b0eedb7a9eb0",
-    measurementId: "G-QBZKLD2Z1K"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBOTN-ktAaGhBMYKy67ANjNF9MFPRLQeIY",
+  authDomain: "brewbud-80fd3.firebaseapp.com",
+  databaseURL: "https://brewbud-80fd3-default-rtdb.firebaseio.com",
+  projectId: "brewbud-80fd3",
+  storageBucket: "brewbud-80fd3.appspot.com",
+  messagingSenderId: "303106774801",
+  appId: "1:303106774801:web:6da98e2516b0eedb7a9eb0",
+  measurementId: "G-QBZKLD2Z1K",
+};
 
-  // Initialize Firebase
-  const fireBaseApp = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(fireBaseApp);
+// Initialize Firebase
+const fireBaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(fireBaseApp);
+const db = getDatabase();
+
+onValue(ref(db),(snapshot) => {
+    const data = snapshot.val();
+    console.log('snapshot', data)
+})
 
 // namespace variable
 app.form = document.querySelector("form");
@@ -107,7 +115,6 @@ app.getCity = (selectInput, userInput) => {
     });
 };
 
-
 // append result from API to the page
 app.displayFunction = (str) => {
   const li = document.createElement("li");
@@ -131,21 +138,23 @@ app.displayFunction = (str) => {
     <p class="phone"> ${phone}</p> <a href="${site}">${site}</a> <button class='listButton'>Button</button>`;
 
   ul.appendChild(li);
-
 };
 
 app.addBreweryToList = (e) => {
-  let arrayToSend = []
+  let arrayToSend = [];
   const brewCard = [...e.target.parentNode.childNodes];
-// let newArr =brewCard.filter((el) => el)
-brewCard.map((info) => {
-  const innerText = info.innerText;
-  return arrayToSend.push(innerText)});
-  const filteredArr = arrayToSend.filter((el) => el && el !== 'Button')
-    const db = getDatabase();
-   set(ref(db), {
-     brewery: filteredArr
-   });
+  // let newArr =brewCard.filter((el) => el)
+  brewCard.map((info) => {
+    const innerText = info.innerText;
+    return arrayToSend.push(innerText);
+  });
+  const filteredArr = arrayToSend.filter((el) => el && el !== "Button");
+  // const db = getDatabase();
+  const postListRef = ref(db);
+  const newPostRef = push(postListRef);
+  set(newPostRef, {
+     filteredArr
+  });
   // console.log("brew",filteredArr);
 };
 // let buttonList = document.querySelectorAll(".listButton");
@@ -174,7 +183,6 @@ const geoCodeUrl = (zip) => {
       app.errorHandlingFunc(err);
     });
 };
-
 
 // eventLister that calls function that make API call
 app.form.addEventListener("submit", function (e) {
@@ -239,6 +247,6 @@ const showRestaurants = (dataSet, ul) => {
   restaurantInfo.innerHTML = `<h3> ${restaurantName}</h3>
     <p>${restaurantAddress}</p>
     <p class="phone"> ${restaurantPhone}</p> <a href="${restaurantSite}">${restaurantSite}</a>
-    <p>${restaurantPrice}</p>;`
+    <p>${restaurantPrice}</p>;`;
   ul.append(restaurantInfo);
 };
