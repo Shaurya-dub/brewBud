569c8b9996b34d444efc43f4e94863836028f900
Shaurya <shauryadubey@hotmail.ca>
2023-05-02 14:16:47 -0400

    further debugging

--- src/firebase.js
+++ src/firebase.js
@@ -24,21 +24,25 @@ import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-let firebaseConfig                   
+let firebaseConfig;
 fetch("/.netlify/functions/fetch-firebase")
-  .then((response) => response.json())
+  .then((response) => {
+    return response.json();
+  })
   .then((data) => {
-    firebaseConfig = data.config_object;
-    const firebaseConfig = {
-      apiKey: data.api_key,
-      authDomain: "brewbud-80fd3.firebaseapp.com",
-      databaseURL: "https://brewbud-80fd3-default-rtdb.firebaseio.com",
-      projectId: "brewbud-80fd",
-      storageBucket: "brewbud-80fd3.appspot.com",
-      messagingSenderId: "303106774801",
-      appId: "1:303106774801:web:6da98e2516b0eedb7a9eb0",
-      measurementId: "G-QBZKLD2Z1K",
-    };
+    // firebaseConfig = data.config_object;
+    console.log('firebase data',data)
+  //  firebaseConfig = {
+  //     apiKey: data.api_key,
+  //     authDomain: "brewbud-80fd3.firebaseapp.com",
+  //     databaseURL: "https://brewbud-80fd3-default-rtdb.firebaseio.com",
+  //     projectId: "brewbud-80fd",
+  //     storageBucket: "brewbud-80fd3.appspot.com",
+  //     messagingSenderId: "303106774801",
+  //     appId: "1:303106774801:web:6da98e2516b0eedb7a9eb0",
+  //     measurementId: "G-QBZKLD2Z1K",
+  //   };
+  firebaseConfig = data.res_object
   });
 
 // Initialize Firebase
