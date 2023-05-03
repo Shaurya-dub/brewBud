ddda653222ef38bda1b00357843abaf4e366b253
Shaurya <shauryadubey@hotmail.ca>
2023-05-03 15:13:21 -0400

    further debugging for netlify deployment

--- src/maps.js
+++ src/maps.js
@@ -30,9 +30,9 @@ async function autoCompleteInput(...inputs) {
   // });
   // console.log('changes')
   await fetch("/.netlify/functions/fetch-maps").catch((e) =>
-    console.error("loading error", e)
+    console.error("maps loading error", e)
   )
-  
+
   inputs.map((input) => {
     const autoComplete = new google.maps.places.Autocomplete(input);
     autoComplete.addListener("place_changed", () => {
