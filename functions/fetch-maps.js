b86757896cd9ce56001279c712b961de763d7f3c
Shaurya <shauryadubey@hotmail.ca>
2023-05-03 15:01:19 -0400

    further debugging for netlify deployment

--- functions/fetch-maps.js
+++ functions/fetch-maps.js
@@ -1,10 +1,17 @@
+import { Loader } from "@googlemaps/js-api-loader";
+
 const handler = async (event) => {
   const api_key = process.env.MAPS_KEY;
-
+ const loader = new Loader({
+   apiKey: api_key,
+   version: "weekly",
+   libraries: ["places", "maps"],
+ });
+ const initLoader = await loader.load()
   try {
     return {
       statusCode: 200,
-      body: JSON.stringify(api_key),
+      body: JSON.stringify(initLoader),
     };
   } catch (error) {
     const { status, statusText, headers, data } = error.response;
